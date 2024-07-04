138994c220b83257c45bd8f958d781e23f218e68
Mike Crowe <drmikecrowe@gmail.com>
2024-07-03 20:05:02 -0400

    adding more xontribs

--- pkgs/by-name/xo/xonsh/xontribs/default.nix
+++ pkgs/by-name/xo/xonsh/xontribs/default.nix
@@ -1,26 +1,47 @@
-{
-  python3,
-}:
-let
+{python3}: let
   callPackage = python3.pkgs.callPackage;
 in {
-  xonsh-direnv = callPackage ./xonsh-direnv { };
+  xonsh-direnv = callPackage ./xonsh-direnv {};
 
-  xontrib-abbrevs = callPackage ./xontrib-abbrevs { };
+  xontrib-abbrevs = callPackage ./xontrib-abbrevs {};
 
-  xontrib-bashisms = callPackage ./xontrib-bashisms { };
+  xontrib-bashisms = callPackage ./xontrib-bashisms {};
 
-  xontrib-debug-tools = callPackage ./xontrib-debug-tools { };
+  xontrib-broot = callPackage ./xontrib-broot {};
 
-  xontrib-distributed = callPackage ./xontrib-distributed { };
+  xontrib-chatgpt = callPackage ./xontrib-chatgpt {};
 
-  xontrib-fish-completer = callPackage ./xontrib-fish-completer { };
+  xontrib-clp = callPackage ./xontrib-clp {};
 
-  xontrib-jedi = callPackage ./xontrib-jedi { };
+  xontrib-debug-tools = callPackage ./xontrib-debug-tools {};
 
-  xontrib-jupyter = callPackage ./xontrib-jupyter { };
+  xontrib-direnv = callPackage ./xontrib-direnv {};
 
-  xontrib-vox = callPackage ./xontrib-vox { };
+  xontrib-distributed = callPackage ./xontrib-distributed {};
 
-  xontrib-whole-word-jumping = callPackage ./xontrib-whole-word-jumping { };
+  xontrib-dot-dot = callPackage ./xontrib-dot-dot {};
+
+  xontrib-fish-completer = callPackage ./xontrib-fish-completer {};
+
+  xontrib-gitinfo = callPackage ./xontrib-gitinfo {};
+
+  xontrib-jedi = callPackage ./xontrib-jedi {};
+
+  xontrib-jupyter = callPackage ./xontrib-jupyter {};
+
+  xontrib-prompt-starship = callPackage ./xontrib-prompt-starship {};
+
+  xontrib-readable-traceback = callPackage ./xontrib-readable-traceback {};
+
+  xontrib-sh = callPackage ./xontrib-sh {};
+
+  xontrib-starship = callPackage ./xontrib-starship {};
+
+  xontrib-term-integrations = callPackage ./xontrib-term-integrations {};
+
+  xontrib-vox = callPackage ./xontrib-vox {};
+
+  xontrib-whole-word-jumping = callPackage ./xontrib-whole-word-jumping {};
+
+  xontrib-zoxide = callPackage ./xontrib-zoxide {};
 }
