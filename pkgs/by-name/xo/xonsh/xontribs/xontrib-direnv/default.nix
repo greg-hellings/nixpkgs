2729344e54120976bb61b66ec7483b64b789cc71
Mike Crowe <drmikecrowe@gmail.com>
2024-07-20 05:40:41 -0400

    "migrating to poetry"

--- pkgs/by-name/xo/xonsh/xontribs/xontrib-direnv/default.nix
+++ pkgs/by-name/xo/xonsh/xontribs/xontrib-direnv/default.nix
@@ -1,20 +1,26 @@
 {
-  pkgs,
-  python3,
+  buildPythonPackage,
+  fetchFromGitHub,
+  lib,
+  direnv,
 }:
-python3.pkgs.buildPythonPackage {
+buildPythonPackage {
   pname = "xontrib-xonsh-direnv";
   version = "1.6.1";
-  src = pkgs.fetchFromGitHub {
+
+  src = fetchFromGitHub {
     owner = "74th";
     repo = "xonsh-direnv";
     rev = "3bea5847b9459c5799c64966ec85e624d0be69b9";
     sha256 = "sha256-h56Gx/MMCW4L6nGwLAhBkiR7bX+qfFk80LEsJMiDtjQ=";
   };
-  propagatedBuildInputs = [pkgs.direnv];
-  meta = {
+
+  propagatedBuildInputs = [direnv];
+
+  meta = with lib; {
     homepage = "https://github.com/74th/xonsh-direnv";
-    license = pkgs.lib.licenses.mit;
-    description = "[how-to use in nix](https://github.com/drmikecrowe/nur-packages) xonsh direnv";
+    license = licenses.mit;
+    maintainers = [maintainers.drmikecrowe];
+    description = "xonsh direnv";
   };
 }
--- pkgs/by-name/xo/xonsh/xontribs/xontrib-term-integrations/default.nix
+++ pkgs/by-name/xo/xonsh/xontribs/xontrib-term-integrations/default.nix
@@ -1,14 +1,19 @@
 {
-  pkgs,
-  python3,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  wheel,
+  poetry-core,
+  pdm-pep517,
+  lib,
 }: let
   pname = "xontrib-term-integrations";
   version = "0.2.0-alpha1";
 in
-  python3.pkgs.buildPythonPackage {
+  buildPythonPackage {
     inherit pname version;
 
-    src = pkgs.fetchFromGitHub {
+    src = fetchFromGitHub {
       owner = "jnoortheen";
       repo = "xontrib-term-integrations";
       rev = "c1cf9a8aa72683548e32b59045917c1c8c760fa8";
@@ -17,28 +22,30 @@ in
 
     doCheck = false;
 
-    nativeBuildInputs = with pkgs.python3Packages; [
+    nativeBuildInputs = [
       setuptools
       wheel
       poetry-core
     ];
 
-    patchPhase = ''
-      echo "from setuptools import setup" > setup.py
-      echo "setup(" >> setup.py
-      echo "    name='${pname}'," >> setup.py
-      echo "    packages=['xontrib', 'xontrib_term_integrations']," >> setup.py
-      echo "    package_dir={'xontrib': 'xontrib', 'xontrib_term_integrations': 'xontrib_term_integrations'}," >> setup.py
-      echo "    package_data={'xontrib': ['*.py'], 'xontrib_term_integrations': ['*.py']}," >> setup.py
-      echo "    zip_safe=False" >> setup.py
-      echo ")" >> setup.py
+    format = "pyproject";
+
+    build-system = [
+      setuptools
+      pdm-pep517
+      poetry-core
+    ];
+
+    postPatch = ''
+      substituteInPlace pyproject.toml \
+      --replace poetry.masonry.api poetry.core.masonry.api \
+      --replace "poetry>=" "poetry-core>="
     '';
 
-    meta = {
+    meta = with lib; {
       homepage = "https://github.com/jnoortheen/xontrib-term-integrations";
-      license = ''
-        MIT
-      '';
-      description = "[how-to use in nix](https://github.com/drmikecrowe/nur-packages) # Terminal Emulators integration";
+      license = licenses.mit;
+      maintainers = [maintainers.drmikecrowe];
+      description = "# Terminal Emulators integration";
     };
   }
